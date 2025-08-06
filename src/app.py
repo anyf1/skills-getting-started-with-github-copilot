4bd538b764af296d4533cc475d278f64752c89a2
anyf1 <2543777469@qq.com>
2025-08-06 03:48:19 +0000

    Exercise: Getting Started with GitHub Copilot Fixes #1

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ app = FastAPI(title="Mergington High School API",
               description="API for viewing and signing up for extracurricular activities")
 
 # Mount the static files directory
+#ayf
 current_dir = Path(__file__).parent
 app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
